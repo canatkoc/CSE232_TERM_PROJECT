5f1c00009f21f28b632b656c83c69aede09a0d62
canatkoc <canat.koc@yeditepe.edu.tr>
2026-05-26 15:39:04 +0300

    delete_gc.c loaded

---
+++ delete_gc.c
@@ -0,0 +1,73 @@
+#include <string.h>
+#include "editor.h"
+
+void deleteLine(int index){
+    int prevIdx;
+    int nextIdx;
+
+    if (index < 0 || index >= MAX_LINES) {
+        return;
+    }
+    if (head == -1) {
+        return; 
+    }
+
+    prevIdx = textbuffer[index].prev;
+    nextIdx = textbuffer[index].next;
+
+    if (prevIdx != -1) {
+        textbuffer[prevIdx].next = nextIdx;
+    } else {
+        head = nextIdx;
+    }
+
+    if (nextIdx != -1) {
+        textbuffer[nextIdx].prev = prevIdx;
+    } else {
+        tail = prevIdx;
+    }
+
+    textbuffer[index].next = -1;
+    textbuffer[index].prev = -1;
+    textbuffer[index].word[0] = '\0';
+}
+
+int garbageCollection(){
+    struct node temp[MAX_LINES];
+    int count = 0;
+    int current = head;
+    int i;
+
+    while (current != -1 && count < MAX_LINES) {
+        strncpy(temp[count].word, textbuffer[current].word, MAX_LEN - 1);
+        temp[count].word[MAX_LEN - 1] = '\0';
+        count++;
+        current = textbuffer[current].next;
+    }
+
+    for (i = 0; i < MAX_LINES; i++) {
+        textbuffer[i].word[0] = '\0';
+        textbuffer[i].next = -1;
+        textbuffer[i].prev = -1;
+    }
+
+    for (i = 0; i < count; i++) {
+        strcpy(textbuffer[i].word, temp[i].word);
+        textbuffer[i].prev = i - 1;
+        textbuffer[i].next = i + 1;
+    }
+
+    if (count > 0) {
+        head = 0;
+        tail = count - 1;
+        textbuffer[head].prev = -1;
+        textbuffer[tail].next = -1;
+        freeIndex = count;
+    } else {
+        head = -1;
+        tail = -1;
+        freeIndex = 0;
+    }
+    return count;
+    
+}
